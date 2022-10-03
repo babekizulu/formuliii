eeb8792d36449efe90f4cb2c77c74e4965f13237
BABEKIZULU <babekizulu@gmail.com>
2022-10-04 00:58:37 +0200

    Setup

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+/*
+* @Author: Lwandle Babekizulu Dlamini
+* @Desc: Primary App Component
+* @Date: 2022/10/03
+*/
+//libs
+import React from 'react';
+//components
+
+const App = () => {
+    return (
+        <div>
+            App
+        </div>
+    );
+};
+
+export default App;
