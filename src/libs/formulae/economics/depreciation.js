a9cb09e1f92e535b1ad14c40be13619f38e8c861
BABEKIZULU <babekizulu@gmail.com>
2022-12-19 12:16:44 +0200

    Setup depreciation.js

---
+++ src/libs/formulae/economics/depreciation.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: babekizulu
+ * @Desc: A library of economics depreciation formulae
+ * @Date: 2022/12/19
+*/
+
+class Depreciation {
+    //straight-line depreciation formula
+    sl() {
+        //
+    }
+    //double-declining balance depreciation formula
+    ddb() {
+        //
+    }
+    //units of production depreciation formula
+    up() {
+        //
+    }
+    //sum of years digits depreciation formula
+    syd() {
+        //
+    }
+}
