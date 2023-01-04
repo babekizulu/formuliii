a664c89c34015ca6b078d9751812a6b65bc6481e
BABEKIZULU <babekizulu@gmail.com>
2023-01-04 02:22:04 +0200

    Completed Physics: Mechanics - velocity equation, acceleration equation, equations of motion & net force equations @TODO: Test equations & complete remaining equations

---
+++ src/libs/formulae/physics/mechanics.js
@@ -0,0 +1,163 @@
+/*
+@Author: Lwandle Babekizulu Dlamini
+@Desc: A library of mechanics formulae
+@Date: 2023/01/04
+@TODO:
+o	Weight
+o	Dry Friction
+o	Centripetal Acceleration
+o	Momentum
+o	Impulse
+o	Impulse-Momentum
+o	Work
+o	Work-Energy
+o	Kinetic Energy
+o	General Potential Energy
+o	Gravitational Potential Energy
+o	Efficiency
+o	Power
+o	Power-Velocity
+o	Angular Velocity
+o	Angular Acceleration
+o	Equations of Rotation
+o	Torque
+o	Second Law of Rotation
+o	Moment of Inertia
+o	Rotational Work
+o	Rotational Power
+o	Rotational Kinetic Energy
+o	Angular Momentum
+o	Angular Impulse
+o	Angular Impulse & Momentum
+o	Universal Gravitation
+o	Gravitational Field
+o	Gravitational Potential Energy
+o	Gravitational Potential
+o	Orbital Speed
+o	Escape Speed
+o	Hooke’s Law
+o	Spring Potential Energy
+o	Simple Harmonic Oscillator 
+o	Simple Pendulum
+o	Frequency
+o	Angular Frequency
+o	Density
+o	Pressure
+o	Pressure in a Fluid
+o	Buoyancy
+o	Mass Flow Rate
+o	Volume Flow Rate
+o	Mass Continuity
+o	Volume Continuity
+o	Bernoulli’s Equation
+o	Dynamic Viscosity
+o	Kinematic Viscosity
+o	Drag
+o	Mach Number
+o	Reynolds Number
+o	Froude Number
+o	Young’s Modulus
+o	Shear Modulus
+o	Bulk Modulus
+o	Surface Tension
+*/
+
+class Mechanics {
+  //velocity equation
+  /*
+    @param: deltaX - Change in Position
+    @param: deltaT - Change in Time
+    */
+  velocity(deltaX, deltaT) {
+    const avV = deltaX / deltaT;
+    return {
+      valName: 'Average Velocity',
+      val: parseFloat(avV).toFixed(3),
+    };
+  }
+
+  //acceleration equation
+  /*
+  @param: deltaV - Change in Velocity
+  @param: deltaT - Change in Time
+  */
+  acceleration(deltaV, deltaT) {
+    const avA = deltaV / deltaT;
+    return {
+      valName: '',
+      val: parseFloat(avA).toFixed(3),
+    };
+  }
+  //equations of motion
+  //1. velocity equation
+  //2. displacement equation
+  //3. velocity^2 equation
+  //4. average velocity equation
+  /*
+  @param: v - velocity
+  @param: v0 - initial velocity
+  @param: a - acceleration
+  @param: t - time
+  @param: d0 - initial position
+  @param: d - displacement
+  */
+
+  equationsOfMotion() {
+    return {
+      velocityEM: function (v0, a, t) {
+        const v = v0 + a * t;
+        return {
+          valName: 'Velocity',
+          val: parseFloat(v).toFixed(3),
+        };
+      },
+      displacement: function (d0, v0, t, a) {
+        const d = d0 + v0 * t + 0.5 * (a * t) ** 2;
+        return {
+          valName: 'Displacement',
+          val: parseFloat(d).toFixed(3),
+        };
+      },
+      velocitySquared: function (v0, a, d, d0) {
+        const sqrtV = Math.sqrt(v0 ** 2 + 2 * a * (d - d0));
+        const vSquared = sqrtV ** 2;
+        return {
+          valName: 'Velocity Squared',
+          val: parseFloat(vSquared).toFloat(3),
+        };
+      },
+      averageVelocity: function (v0, v) {
+        const av = 0.5 * (v + v0);
+        return {
+          valName: 'Average Velocity',
+          val: parseFloat(av).toFixed(3),
+        };
+      },
+    };
+  }
+  //net force equations
+  /*
+  @param: m - mass
+  @param: a - acceleration
+  */
+  netForce1(m, a) {
+    const nf = m * a;
+    return {
+      valName: 'Net Force',
+      val: parseFloat(nf).toFixed(3),
+    };
+  }
+
+  /*
+  @param: deltaP - Change in Momentum
+  @param: deltaT - Change in Time
+  */
+  netForce2(deltaP, deltaT) {
+    const nf = deltaP / deltaT;
+    return {
+      valName: 'Net Force',
+      val: parseFloat(nf).toFixed(3),
+    };
+  }
+}
+module.exports = Mechanics;
---
+++ src/tests/physics/mechanics.test.js
@@ -0,0 +1,10 @@
+/*
+@Author: Lwandle Babekizulu Dlamini
+@Desc: testing file for mechanics formulae
+@Date: 2023/01/04
+@TODO:
+- test velocity equations
+- test acceleration equations
+- test equations of motion
+- test net force equations
+*/
